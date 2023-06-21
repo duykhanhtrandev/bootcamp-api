86b2a08a1608e10a2701274a7f80d54fbd1b4466
Duy Khánh <109634649+duykhanhtrandev@users.noreply.github.com>
2023-06-21 10:46:18 +0700

    mongoose error handling

--- controllers/bootcampController.js
+++ controllers/bootcampController.js
@@ -1,7 +1,8 @@
+const ErrorResponse = require("../utils/errorResponse");
 const { StatusCodes } = require("http-status-codes");
 const BootCamp = require("../models/bootcampModel");
 
-const createBootcamp = async (req, res) => {
+const createBootcamp = async (req, res, next) => {
   try {
     const bootcamp = await BootCamp.create(req.body);
     res.status(StatusCodes.CREATED).json({
@@ -9,18 +10,18 @@ const createBootcamp = async (req, res) => {
       data: bootcamp,
     });
   } catch (error) {
-    res.status(StatusCodes.BAD_REQUEST).json({ success: false });
+    next(error);
   }
 };
 
-const getAllBootcamps = async (req, res) => {
+const getAllBootcamps = async (req, res, next) => {
   try {
     const bootcamps = await BootCamp.find().sort("-createdAt");
     res
       .status(StatusCodes.OK)
       .json({ success: true, count: bootcamps.length, data: bootcamps });
   } catch (error) {
-    res.status(StatusCodes.NOT_FOUND).json({ success: false });
+    next(error);
   }
 };
 
@@ -28,39 +29,53 @@ const getSingleBootcamp = async (req, res, next) => {
   try {
     const bootcamp = await BootCamp.findById(req.params.id);
     if (!bootcamp) {
-      return res.status(StatusCodes.BAD_REQUEST).json({ success: false });
+      return next(
+        new ErrorResponse(
+          `Bootcamp not found with id of ${req.params.id}`,
+          StatusCodes.NOT_FOUND
+        )
+      );
     }
     res.status(StatusCodes.OK).json({ success: true, data: bootcamp });
   } catch (error) {
-    // res.status(StatusCodes.BAD_REQUEST).json({ success: false });
     next(error);
   }
 };
 
-const updateBootcamp = async (req, res) => {
+const updateBootcamp = async (req, res, next) => {
   try {
     const bootcamp = await BootCamp.findByIdAndUpdate(req.params.id, req.body, {
       new: true,
       runValidators: true,
     });
     if (!bootcamp) {
-      return res.status(StatusCodes.NOT_FOUND).json({ success: false });
+      return next(
+        new ErrorResponse(
+          `Bootcamp not found with id of ${req.params.id}`,
+          StatusCodes.NOT_FOUND
+        )
+      );
     }
     res.status(StatusCodes.OK).json({ success: true, data: bootcamp });
   } catch (error) {
-    res.status(StatusCodes.BAD_REQUEST).json({ success: false });
+    next(error);
   }
 };
 
-const deleteBootcamp = async (req, res) => {
+const deleteBootcamp = async (req, res, next) => {
   try {
     const bootcamp = await BootCamp.findByIdAndDelete(req.params.id);
     if (!bootcamp) {
-      return res.status(StatusCodes.NOT_FOUND).json({ success: false });
+      return next(
+        new ErrorResponse(
+          `Bootcamp not found with id of ${req.params.id}`,
+          StatusCodes.NOT_FOUND
+        )
+      );
     }
     res.status(StatusCodes.OK).json({ success: true, data: {} });
   } catch (error) {
-    res.status(StatusCodes.BAD_REQUEST).json({ success: false });
+    next(error);
   }
 };
 
--- middleware/error.js
+++ middleware/error.js
@@ -1,9 +1,33 @@
+const { StatusCodes } = require("http-status-codes");
+const ErrorResponse = require("../utils/errorResponse");
 const errorHandler = (err, req, res, next) => {
-  console.log(err.stack.red);
+  let error = { ...err };
 
-  res.status(500).json({
+  error.message = err.message;
+
+  console.log(err);
+
+  // Mongoose bad ObjectId
+  if (err.name === "CastError") {
+    const message = `Resource not found with id of ${err.value}`;
+    error = new ErrorResponse(message, StatusCodes.NOT_FOUND);
+  }
+
+  // Mongoose duplicate key
+  if (err.code === 11000) {
+    const message = "Duplicate field value entered";
+    error = new ErrorResponse(message, StatusCodes.BAD_REQUEST);
+  }
+
+  // Mongoose validate error
+  if (err.name === "ValidationError") {
+    const message = Object.values(err.errors).map((val) => val.message);
+    error = new ErrorResponse(message, StatusCodes.BAD_REQUEST);
+  }
+
+  res.status(error.statusCode || 500).json({
     success: false,
-    error: err.message,
+    error: error.message || "Server Error",
   });
 };
 
