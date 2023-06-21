df727dceb7020c687b663076500b490eef74f70a
Duy Khánh <109634649+duykhanhtrandev@users.noreply.github.com>
2023-06-21 11:20:43 +0700

    async await middleware

--- controllers/bootcampController.js
+++ controllers/bootcampController.js
@@ -1,83 +1,64 @@
 const ErrorResponse = require("../utils/errorResponse");
+const asyncHandler = require("../middleware/async");
 const { StatusCodes } = require("http-status-codes");
 const BootCamp = require("../models/bootcampModel");
 
-const createBootcamp = async (req, res, next) => {
-  try {
-    const bootcamp = await BootCamp.create(req.body);
-    res.status(StatusCodes.CREATED).json({
-      success: true,
-      data: bootcamp,
-    });
-  } catch (error) {
-    next(error);
-  }
-};
+const createBootcamp = asyncHandler(async (req, res, next) => {
+  const bootcamp = await BootCamp.create(req.body);
+  res.status(StatusCodes.CREATED).json({
+    success: true,
+    data: bootcamp,
+  });
+});
 
-const getAllBootcamps = async (req, res, next) => {
-  try {
-    const bootcamps = await BootCamp.find().sort("-createdAt");
-    res
-      .status(StatusCodes.OK)
-      .json({ success: true, count: bootcamps.length, data: bootcamps });
-  } catch (error) {
-    next(error);
-  }
-};
+const getAllBootcamps = asyncHandler(async (req, res, next) => {
+  const bootcamps = await BootCamp.find().sort("-createdAt");
+  res
+    .status(StatusCodes.OK)
+    .json({ success: true, count: bootcamps.length, data: bootcamps });
+});
 
-const getSingleBootcamp = async (req, res, next) => {
-  try {
-    const bootcamp = await BootCamp.findById(req.params.id);
-    if (!bootcamp) {
-      return next(
-        new ErrorResponse(
-          `Bootcamp not found with id of ${req.params.id}`,
-          StatusCodes.NOT_FOUND
-        )
-      );
-    }
-    res.status(StatusCodes.OK).json({ success: true, data: bootcamp });
-  } catch (error) {
-    next(error);
+const getSingleBootcamp = asyncHandler(async (req, res, next) => {
+  const bootcamp = await BootCamp.findById(req.params.id);
+  if (!bootcamp) {
+    return next(
+      new ErrorResponse(
+        `Bootcamp not found with id of ${req.params.id}`,
+        StatusCodes.NOT_FOUND
+      )
+    );
   }
-};
+  res.status(StatusCodes.OK).json({ success: true, data: bootcamp });
+});
 
-const updateBootcamp = async (req, res, next) => {
-  try {
-    const bootcamp = await BootCamp.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    });
-    if (!bootcamp) {
-      return next(
-        new ErrorResponse(
-          `Bootcamp not found with id of ${req.params.id}`,
-          StatusCodes.NOT_FOUND
-        )
-      );
-    }
-    res.status(StatusCodes.OK).json({ success: true, data: bootcamp });
-  } catch (error) {
-    next(error);
+const updateBootcamp = asyncHandler(async (req, res, next) => {
+  const bootcamp = await BootCamp.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+  if (!bootcamp) {
+    return next(
+      new ErrorResponse(
+        `Bootcamp not found with id of ${req.params.id}`,
+        StatusCodes.NOT_FOUND
+      )
+    );
   }
-};
+  res.status(StatusCodes.OK).json({ success: true, data: bootcamp });
+});
 
-const deleteBootcamp = async (req, res, next) => {
-  try {
-    const bootcamp = await BootCamp.findByIdAndDelete(req.params.id);
-    if (!bootcamp) {
-      return next(
-        new ErrorResponse(
-          `Bootcamp not found with id of ${req.params.id}`,
-          StatusCodes.NOT_FOUND
-        )
-      );
-    }
-    res.status(StatusCodes.OK).json({ success: true, data: {} });
-  } catch (error) {
-    next(error);
+const deleteBootcamp = asyncHandler(async (req, res, next) => {
+  const bootcamp = await BootCamp.findByIdAndDelete(req.params.id);
+  if (!bootcamp) {
+    return next(
+      new ErrorResponse(
+        `Bootcamp not found with id of ${req.params.id}`,
+        StatusCodes.NOT_FOUND
+      )
+    );
   }
-};
+  res.status(StatusCodes.OK).json({ success: true, data: {} });
+});
 
 module.exports = {
   getAllBootcamps,
