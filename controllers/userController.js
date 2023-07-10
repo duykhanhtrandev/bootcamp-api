40866d54046c366c04e0753d2be363bec1fbff83
Duy Khánh <109634649+duykhanhtrandev@users.noreply.github.com>
2023-07-10 10:01:11 +0700

    admin users crud

--- controllers/userController.js
+++ controllers/userController.js
@@ -16,6 +16,51 @@ const getCurrentUser = asyncHandler(async (req, res, next) => {
   });
 });
 
+const getAllUsers = asyncHandler(async (req, res, next) => {
+  res.status(StatusCodes.OK).json(res.advancedResults);
+});
+
+const getSingleUser = asyncHandler(async (req, res, next) => {
+  const user = await User.findOne({ _id: req.params.id });
+
+  if (!user) {
+    return next(
+      new ErrorResponse(
+        `User not found with id of ${req.params.id}`,
+        StatusCodes.NOT_FOUND
+      )
+    );
+  }
+
+  res.status(StatusCodes.OK).json({ success: true, data: user });
+});
+
+const createUser = asyncHandler(async (req, res, next) => {
+  const user = await User.create(req.body);
+
+  res.status(StatusCodes.OK).json({ success: true, data: user });
+});
+
+const updateUser = asyncHandler(async (req, res, next) => {
+  const user = await User.findOneAndUpdate({ _id: req.params.id }, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(StatusCodes.OK).json({ success: true, data: user });
+});
+
+const deleteUser = asyncHandler(async (req, res, next) => {
+  const user = await User.findOneAndDelete({ _id: req.params.id });
+
+  res.status(StatusCodes.OK).json({ success: true, data: {} });
+});
+
 module.exports = {
   getCurrentUser,
+  getAllUsers,
+  getSingleUser,
+  createUser,
+  updateUser,
+  deleteUser,
 };
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,10 +1,27 @@
 const express = require("express");
 const router = express.Router();
 
-const { getCurrentUser } = require("../controllers/userController");
+const {
+  getCurrentUser,
+  getAllUsers,
+  getSingleUser,
+  createUser,
+  updateUser,
+  deleteUser,
+} = require("../controllers/userController");
 
-const { protect } = require("../middleware/auth");
+const User = require("../models/userModel");
 
-router.route("/me").get(protect, getCurrentUser);
+const { protect, authorize } = require("../middleware/auth");
+const advancedResults = require("../middleware/advancedResults");
+
+router.use(protect);
+
+router.route("/me").get(getCurrentUser);
+
+router.use(authorize("admin"));
+
+router.route("/").get(advancedResults(User), getAllUsers).post(createUser);
+router.route("/:id").get(getSingleUser).patch(updateUser).delete(deleteUser);
 
 module.exports = router;
